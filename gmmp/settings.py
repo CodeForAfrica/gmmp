98736b34614fa4ca9a83007d8f99fb60f8aba838
Adi Eyal <adi@code4sa.org>
2015-03-02 18:37:26 +0000

    db settings update

--- gmmp/settings.py
+++ gmmp/settings.py
@@ -30,7 +30,7 @@ if DEBUG:
 }
 else:
     import dj_database_url
-    DATABASES['default'] =  dj_database_url.config()
+    DATABASES = {'default' : dj_database_url.config()}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
