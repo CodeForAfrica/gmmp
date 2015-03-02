d5fe7e096244d8dd9056ac257e7e78ef85e8f219
Adi Eyal <adi@code4sa.org>
2015-03-02 14:47:42 +0000

    Added migrations - also changed db to postgres

---
+++ gmmp/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import django_countries.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Monitor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('country', django_countries.fields.CountryField(max_length=2)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- gmmp/settings.py
+++ gmmp/settings.py
@@ -70,8 +70,11 @@ WSGI_APPLICATION = 'gmmp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gmmp',
+        'USER': 'gmmp',
+        'PASSWORD': 'gmmp',
+        'HOST': 'localhost'
     }
 }
 
