0f04b23776b3fb38ab397c7c82890b9d214f1c2d
Adi Eyal <adi@code4sa.org>
2015-03-02 18:14:38 +0000

    redirect root to admin url

--- gmmp/urls.py
+++ gmmp/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
 )
