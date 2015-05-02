63184bb7f8450d6f553e7a608845319041d8251d
Adi Eyal <adi@code4sa.org>
2015-05-02 21:39:26 +0000

    Removed traces of django-guardian

--- gmmp/settings.py
+++ gmmp/settings.py
@@ -61,7 +61,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'guardian', 
 	'forms',
 	'gmmp',
 	'django_countries',
@@ -69,7 +68,6 @@ INSTALLED_APPS = (
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
-    'guardian.backends.ObjectPermissionBackend',
 )
 
 ANONYMOUS_USER_ID = -1
