cf539ae3a10e7f553c5edf6dff753443eb4f8d32
xybrnet <damian@code4sa.org>
2015-05-06 10:53:41 +0200

    Get basic xlsx generation working

--- reports/views.py
+++ reports/views.py
@@ -1,14 +1,20 @@
+# Django
 from django.views.generic import View
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# 3rd Party
+import xlsxwriter
+
+# Project
 from reports.forms import ReportFilterForm
+from reports.report_builder import XLSXReportBuilder
 
 class ReportView(View):
     template_name = 'report_filter.html'
 
     def get(self, request, *args, **kwargs):
         filter_form = ReportFilterForm()
-        import ipdb; ipdb.set_trace()
         context = {'form' : filter_form}
         return render(
             request,
@@ -16,9 +22,19 @@ class ReportView(View):
             context)
 
     def post(self, request, *args, **kwargs):
-        import ipdb; ipdb.set_trace()
         filter_form = ReportFilterForm(request.POST)
         if filter_form.is_valid():
+            # country = filter_form.cleaned_data['country']
+            # wb = xlsxwriter.Workbook('GMMP Report - %s.xlsx' % country)
+            # ws = wb.add_worksheet('Countries per medium')
+            xlsx = XLSXReportBuilder(filter_form).build()
+
+            # Where should filename should be determined?
+
+            filename = 'Report'
+            response = HttpResponse(xlsx, content_type='application/vnd.ms-excel')
+            response['Content-Disposition'] = 'attachment; filename=%s.xlsx' % filename
+            return response
 
         report_filter = ReportFilterForm()
         context = {'form' : filter_form}
