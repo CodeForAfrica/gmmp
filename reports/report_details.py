aca2aff3fcd1a71fafe70fd39421c6b7bc1ac554
xybrnet <damian@code4sa.org>
2015-05-25 15:30:22 +0200

    Add ws-17

--- reports/report_details.py
+++ reports/report_details.py
@@ -1,14 +1,16 @@
-worksheets = {
-    'ws_01': {
-        'ws_name': '',
-        'title': '',
-        'description': '',
-        'function': ''
-    },
+WS_INFO = {
     'ws_16': {
-        'name': 'Subject function by sex by occ',
+        'name': '16-Function by sex by occ',
         'title': 'Function of news subjects by sex - by occupation',
         'desc': 'Breakdown of  Function of news subjects by sex - by occupation',
         'function': 'ws_16_subject_function_by_sex_by_occupation'
     },
+    'ws_17': {
+        'name': '17-Function by sex by age',
+        'title': 'Function of news subjects by sex - by age',
+        'desc': 'Breakdown of  Function of news subjects by sex - by age',
+        'function': 'ws_17_subject_function_by_sex_by_age'
+    }
 }
+
+
