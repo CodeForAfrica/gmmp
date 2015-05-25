d2c3521ae1a44a809dcfb34601516c864ec47fac
xybrnet <damian@code4sa.org>
2015-05-25 14:47:08 +0200

    Create report details file

---
+++ reports/report_details.py
@@ -0,0 +1,14 @@
+worksheets = {
+    'ws_01': {
+        'ws_name': '',
+        'title': '',
+        'description': '',
+        'function': ''
+    },
+    'ws_16': {
+        'name': 'Subject function by sex by occ',
+        'title': 'Function of news subjects by sex - by occupation',
+        'desc': 'Breakdown of  Function of news subjects by sex - by occupation',
+        'function': 'ws_16_subject_function_by_sex_by_occupation'
+    },
+}
