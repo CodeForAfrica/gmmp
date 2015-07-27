1a884b8a1dbb36cea0f9389f57423f1fed04acbd
Damian <damian@code4sa.org>
2015-07-27 17:21:16 +0200

    Add clean_title exception

--- reports/report_builder.py
+++ reports/report_builder.py
@@ -82,7 +82,9 @@ def clean_title(text):
     """
     Return the string passed in stripped of its numbers and parentheses
     """
-    return text[text.find(')')+1:].lstrip()
+    if text != "Congo (the Democratic Republic of the)":
+        return text[text.find(')')+1:].lstrip()
+    return text
 
 class XLSXDataExportBuilder():
     def __init__(self, request):
@@ -423,33 +425,31 @@ class XLSXReportBuilder:
         self.P = workbook.add_format(FORMATS['P'])
 
         # Use the following for specifying which reports to create durin dev
-        test_functions = [
-            'ws_01', 'ws_02', 'ws_04', 'ws_05', 'ws_06', 'ws_07', 'ws_08', 'ws_09', 'ws_10',
-            'ws_11', 'ws_12', 'ws_13', 'ws_14', 'ws_15', 'ws_16', 'ws_17', 'ws_18', 'ws_19', 'ws_20',
-            'ws_21', 'ws_23', 'ws_24', 'ws_25', 'ws_26', 'ws_27', 'ws_28', 'ws_29', 'ws_30',
-            'ws_31', 'ws_32', 'ws_34', 'ws_35', 'ws_36', 'ws_38', 'ws_39', 'ws_40',
-            'ws_41', 'ws_42', 'ws_43', 'ws_44', 'ws_45', 'ws_46', 'ws_47', 'ws_48',]
+        # test_functions = [
+        #     'ws_01', 'ws_02', 'ws_04', 'ws_05', 'ws_06', 'ws_07', 'ws_08', 'ws_09', 'ws_10',
+        #     'ws_11', 'ws_12', 'ws_13', 'ws_14', 'ws_15', 'ws_16', 'ws_17', 'ws_18', 'ws_19', 'ws_20',
+        #     'ws_21', 'ws_23', 'ws_24', 'ws_25', 'ws_26', 'ws_27', 'ws_28', 'ws_29', 'ws_30',
+        #     'ws_31', 'ws_32', 'ws_34', 'ws_35', 'ws_36', 'ws_38', 'ws_39', 'ws_40',
+        #     'ws_41', 'ws_42', 'ws_43', 'ws_44', 'ws_45', 'ws_46', 'ws_47', 'ws_48',]
 
-        # test_functions = ['ws_01', 'ws_04', 'ws_12', 'ws_46', 'ws_30']
+        # sheet_info = OrderedDict(sorted(WS_INFO.items(), key=lambda t: t[0]))
 
-        sheet_info = OrderedDict(sorted(WS_INFO.items(), key=lambda t: t[0]))
-
-        for function in test_functions:
-            if self.report_type in sheet_info[function]['reports']:
-                ws = workbook.add_worksheet(sheet_info[function]['name'])
-                self.write_headers(ws, sheet_info[function]['title'], sheet_info[function]['desc'])
-                getattr(self, function)(ws)
+        # for function in test_functions:
+        #     if self.report_type in sheet_info[function]['reports']:
+        #         ws = workbook.add_worksheet(sheet_info[function]['name'])
+        #         self.write_headers(ws, sheet_info[function]['title'], sheet_info[function]['desc'])
+        #         getattr(self, function)(ws)
 
         # -------------------------------------------------------------------
 
         # To ensure ordered worksheets
-        # sheet_info = OrderedDict(sorted(WS_INFO.items(), key=lambda t: t[0]))
+        sheet_info = OrderedDict(sorted(WS_INFO.items(), key=lambda t: t[0]))
 
-        # for ws_num, ws_info in sheet_info.iteritems():
-        #     if self.report_type in ws_info['reports']:
-        #         ws = workbook.add_worksheet(ws_info['name'])
-        #         self.write_headers(ws, ws_info['title'], ws_info['desc'])
-        #         getattr(self, ws_num)(ws)
+        for ws_num, ws_info in sheet_info.iteritems():
+            if self.report_type in ws_info['reports']:
+                ws = workbook.add_worksheet(ws_info['name'])
+                self.write_headers(ws, ws_info['title'], ws_info['desc'])
+                getattr(self, ws_num)(ws)
 
         workbook.close()
         output.seek(0)
