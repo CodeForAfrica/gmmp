79f05fe0655ab27d54225582ce25ec397872d55a
Clemence Kyara <clemence@codeforafrica.org>
2020-02-10 09:52:43 +0300

    Add initial custom Add**Submission dashboard modules

--- gmmp/dashboard.py
+++ gmmp/dashboard.py
@@ -2,39 +2,32 @@ from django.utils.translation import ugettext_lazy as _
 from jet.dashboard import modules
 from jet.dashboard.dashboard import Dashboard, AppIndexDashboard
 
+from gmmp.dashboard_modules import AddInternetNewsSubmission, AddNewspaperSubmission, AddRadioSubmission, AddTelevisionSubmission, AddTwitterSubmission
 
 class CustomIndexDashboard(Dashboard):
-    columns = 5
+    columns = 3
 
     def init_with_context(self, context):
         self.available_children.append(modules.LinkList)
-        self.children.append(modules.LinkList(
-            _('Add News Subbmission'),
-            children=[
-            ],
+        self.children.append(AddNewspaperSubmission(
             column=0,
             order=0
         ))
-        self.children.append(modules.LinkList(
-            _('Add News Subbmission'),
-            children=[
-            ],
+        self.children.append(AddRadioSubmission(
             column=1,
             order=0
         ))
-        self.children.append(modules.LinkList(
-            _('Add News Subbmission'),
-            children=[
-            ],
+        self.children.append(AddTelevisionSubmission(
             column=2,
             order=0
         ))
-        self.children.append(modules.LinkList(
-            _('Add News Subbmission'),
-            children=[
-            ],
-            column=3,
-            order=0
+        self.children.append(AddInternetNewsSubmission(
+            column=0,
+            order=1
+        ))
+        self.children.append(AddTwitterSubmission(
+            column=1,
+            order=1
         ))
         self.children.append(modules.LinkList(
             _('Add News Subbmission'),
---
+++ gmmp/dashboard_modules.py
@@ -0,0 +1,81 @@
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from jet.dashboard.modules import DashboardModule
+
+from forms.models import (
+    InternetNewsSheet,
+    NewspaperSheet,
+    RadioSheet,
+    TelevisionSheet,
+    TwitterSheet,
+)
+
+
+class AddInternetNewsSubmission(DashboardModule):
+    title = _("Add Internet News Submissions")
+    url = reverse("admin:forms_internetnewssheet_add")
+    template = "gmmp/dashboard_modules/add_submission.html"
+
+    def init_with_context(self, context):
+        self.children = [
+            {
+                "title": _("Add Internet News Submissions"),
+                "url": reverse("admin:forms_internetnewssheet_add"),
+            },
+        ]
+
+
+class AddNewspaperSubmission(DashboardModule):
+    title = _("Add Newspaper Submissions")
+    url = reverse("admin:forms_newspapersheet_add")
+    template = "gmmp/dashboard_modules/add_submission.html"
+
+    def init_with_context(self, context):
+        self.children = [
+            {
+                "title": _("Add Newspaper Submissions"),
+                "url": reverse("admin:forms_newspapersheet_add"),
+            },
+        ]
+
+
+class AddRadioSubmission(DashboardModule):
+    title = _("Add Radio Submissions")
+    url = reverse("admin:forms_radiosheet_add")
+    template = "gmmp/dashboard_modules/add_submission.html"
+
+    def init_with_context(self, context):
+        self.children = [
+            {
+                "title": _("Add Radio Submissions"),
+                "url": reverse("admin:forms_radiosheet_add"),
+            },
+        ]
+
+
+class AddTelevisionSubmission(DashboardModule):
+    title = _("Add Television Submissions")
+    url = reverse("admin:forms_televisionsheet_add")
+    template = "gmmp/dashboard_modules/add_submission.html"
+
+    def init_with_context(self, context):
+        self.children = [
+            {
+                "title": _("Add Television Submissions"),
+                "url": reverse("admin:forms_televisionsheet_add"),
+            },
+        ]
+
+
+class AddTwitterSubmission(DashboardModule):
+    title = _("Add Twitter Submissions")
+    url = reverse("admin:forms_twittersheet_add")
+    template = "gmmp/dashboard_modules/add_submission.html"
+
+    def init_with_context(self, context):
+        self.children = [
+            {
+                "title": _("Add Twitter Submissions"),
+                "url": reverse("admin:forms_twittersheet_add"),
+            },
+        ]
