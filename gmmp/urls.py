3c1fa6e8020848f9930e8b8049697bd77f02e3a5
Clemence Kyara <clemence@codeforafrica.org>
2020-02-17 16:37:06 +0300

    Enable internationalization of URL patterns

--- gmmp/urls.py
+++ gmmp/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -12,34 +13,35 @@ admin.site.site_title = settings.ADMIN_SITE_SITE_TITLE
 admin.site.site_url = settings.ADMIN_SITE_SITE_URL
 admin.site.index_title = settings.ADMIN_SITE_INDEX_TITLE
 
-urlpatterns = [
-    path('grappelli/', include('grappelli.urls')), # grappelli URLS
-    re_path(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    re_path(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+urlpatterns = i18n_patterns(
+    path("grappelli/", include("grappelli.urls")),  # grappelli URLS
+    re_path(r"^jet/", include("jet.urls", "jet")),  # Django JET URLS
+    re_path(
+        r"^jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")
+    ),  # Django JET dashboard URLS
     path(
-        'admin/password_reset/',
+        "admin/password_reset/",
         auth_views.PasswordResetView.as_view(),
-        name='admin_password_reset',
+        name="admin_password_reset",
     ),
     path(
-        'admin/password_reset/done/',
+        "admin/password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done',
+        name="password_reset_done",
     ),
     path(
-        'reset/<uidb64>/<token>/',
+        "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm',
+        name="password_reset_confirm",
     ),
     path(
-        'reset/done/',
+        "reset/done/",
         auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete',
+        name="password_reset_complete",
     ),
-    path('admin/', admin.site.urls), # Admin site URLS
-
-    re_path(r'^$', RedirectView.as_view(url='/admin'), name='go-to-admin'),
-
+    path("admin/", admin.site.urls),  # Admin site URLS
+    re_path(r"^$", RedirectView.as_view(url="/admin"), name="go-to-admin"),
     # TODO: Fix reports views, they're blocking migration
     # url(r'^reports/', include('reports.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    prefix_default_language=False
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
