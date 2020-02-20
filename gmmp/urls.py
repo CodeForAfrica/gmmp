23452d1b1e4dfb256c5052fd4776a462753ad7cf
Clemence Kyara <clemence@codeforafrica.org>
2020-02-20 10:59:59 +0300

    Add language switching to urls

--- gmmp/urls.py
+++ gmmp/urls.py
@@ -13,37 +13,41 @@ admin.site.site_title = settings.ADMIN_SITE_SITE_TITLE
 admin.site.site_url = settings.ADMIN_SITE_SITE_URL
 admin.site.index_title = settings.ADMIN_SITE_INDEX_TITLE
 
-urlpatterns = i18n_patterns(
-    # grappelli URLS
-    path("grappelli/", include("grappelli.urls")),
-    # Django JET URLS
-    re_path(r"^jet/", include("jet.urls", "jet")),
-    # Django JET dashboard URLS
-    re_path(r"^jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")),
-    path(
-        "admin/password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="admin_password_reset",
-    ),
-    path(
-        "admin/password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
-    # Admin site URLS
-    path("admin/", admin.site.urls),
-    re_path(r"^$", RedirectView.as_view(url="/admin"), name="go-to-admin"),
-    # TODO: Fix reports views, they're blocking migration
-    # url(r'^reports/', include('reports.urls')),
-    prefix_default_language=False,
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    i18n_patterns(
+        # grappelli URLS
+        path("grappelli/", include("grappelli.urls")),
+        # Django JET URLS
+        re_path(r"^jet/", include("jet.urls", "jet")),
+        # Django JET dashboard URLS
+        re_path(r"^jet/dashboard/", include("jet.dashboard.urls", "jet-dashboard")),
+        path(
+            "admin/password_reset/",
+            auth_views.PasswordResetView.as_view(),
+            name="admin_password_reset",
+        ),
+        path(
+            "admin/password_reset/done/",
+            auth_views.PasswordResetDoneView.as_view(),
+            name="password_reset_done",
+        ),
+        path(
+            "reset/<uidb64>/<token>/",
+            auth_views.PasswordResetConfirmView.as_view(),
+            name="password_reset_confirm",
+        ),
+        path(
+            "reset/done/",
+            auth_views.PasswordResetCompleteView.as_view(),
+            name="password_reset_complete",
+        ),
+        # Admin site URLS
+        path("admin/", admin.site.urls),
+        re_path(r"^$", RedirectView.as_view(url="/admin"), name="go-to-admin"),
+        # TODO: Fix reports views, they're blocking migration
+        # url(r'^reports/', include('reports.urls')),
+        prefix_default_language=False,
+    )
+    + [path("i18n/", include("django.conf.urls.i18n"))]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
