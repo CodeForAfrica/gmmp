658ff4d5048f82a29b9f56e4cf0c9f490dfb1c61
esir <esirkings@gmail.com>
2020-08-27 08:15:40 +0300

    Use logical or instead of bitwise or

--- forms/static/forms/admin/dependency_fields.js
+++ forms/static/forms/admin/dependency_fields.js
@@ -5,7 +5,7 @@
         let tv_person = $('#id_televisionperson_set-0-victim_or_survivor_1:checked').length;
         let internent_person = $('#id_internetnewsperson_set-0-victim_or_survivor_1:checked').length;
 
-        if((newspaper_person > 0) | (radio_person > 0) | (tv_person > 0) | (internent_person > 0)){
+        if((newspaper_person > 0) || (radio_person > 0) || (tv_person > 0) || (internent_person > 0)){
             $('.field-victim_of').hide();
             $('.field-survivor_of').hide();
         }
@@ -16,7 +16,7 @@
         let tv_person = $('#id_televisionperson_set-0-victim_or_survivor_1:checked').length;
         let internent_person = $('#id_internetnewsperson_set-0-victim_or_survivor_1:checked').length;
 
-        if((newspaper_person > 0) | (radio_person > 0) | (tv_person > 0) | (internent_person > 0)){
+        if((newspaper_person > 0) || (radio_person > 0) || (tv_person > 0) || (internent_person > 0)){
             $('.field-victim_of').hide();
             $('.field-survivor_of').hide();
         }else{
