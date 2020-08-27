1883d60fe70badf628cd87655873e8595542b501
esir <esirkings@gmail.com>
2020-08-27 08:05:38 +0300

    Use snake case for variables

--- forms/static/forms/admin/dependency_fields.js
+++ forms/static/forms/admin/dependency_fields.js
@@ -1,27 +1,27 @@
 (function($){
     $(document).ready(function() {
-        let newspaperperson = $('#id_newspaperperson_set-0-victim_or_survivor_1:checked').length
-        let radioperson = $('#id_radioperson_set-0-victim_or_survivor_1:checked').length
-        let tvperson = $('#id_televisionperson_set-0-victim_or_survivor_1:checked').length
-        let internentperson = $('#id_internetnewsperson_set-0-victim_or_survivor_1:checked').length
+        let newspaper_person = $('#id_newspaperperson_set-0-victim_or_survivor_1:checked').length;
+        let radio_person = $('#id_radioperson_set-0-victim_or_survivor_1:checked').length;
+        let tv_person = $('#id_televisionperson_set-0-victim_or_survivor_1:checked').length;
+        let internent_person = $('#id_internetnewsperson_set-0-victim_or_survivor_1:checked').length;
 
-        if((newspaperperson > 0) | (radioperson > 0) | (tvperson > 0) | (internentperson > 0)){
+        if((newspaper_person > 0) | (radio_person > 0) | (tv_person > 0) | (internent_person > 0)){
             $('.field-victim_of').hide();
-            $('.field-survivor_of').hide()
+            $('.field-survivor_of').hide();
         }
     });
     $(document).change(function() {
-        let newspaperperson = $('#id_newspaperperson_set-0-victim_or_survivor_1:checked').length
-        let radioperson = $('#id_radioperson_set-0-victim_or_survivor_1:checked').length
-        let tvperson = $('#id_televisionperson_set-0-victim_or_survivor_1:checked').length
-        let internentperson = $('#id_internetnewsperson_set-0-victim_or_survivor_1:checked').length
+        let newspaper_person = $('#id_newspaperperson_set-0-victim_or_survivor_1:checked').length;
+        let radio_person = $('#id_radioperson_set-0-victim_or_survivor_1:checked').length;
+        let tv_person = $('#id_televisionperson_set-0-victim_or_survivor_1:checked').length;
+        let internent_person = $('#id_internetnewsperson_set-0-victim_or_survivor_1:checked').length;
 
-        if((newspaperperson > 0) | (radioperson > 0) | (tvperson > 0) | (internentperson > 0)){
+        if((newspaper_person > 0) | (radio_person > 0) | (tv_person > 0) | (internent_person > 0)){
             $('.field-victim_of').hide();
-            $('.field-survivor_of').hide()
+            $('.field-survivor_of').hide();
         }else{
             $('.field-victim_of').show();
-            $('.field-survivor_of').show()
+            $('.field-survivor_of').show();
         }
     });
 }(jet.jQuery));
