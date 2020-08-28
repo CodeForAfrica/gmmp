c6d1b10811d2296b08ec8145f17c204e049c9bb3
esir <esirkings@gmail.com>
2020-08-28 09:50:19 +0300

    Add comment on the changes made.

--- reports/forms.py
+++ reports/forms.py
@@ -6,6 +6,13 @@ class GlobalForm(forms.Form):
     pass
 
 class CountryForm(forms.Form):
+    """
+    Due to the system checks added in django 1.9 https://docs.djangoproject.com/en/3.1/releases/1.9/#urls,
+    this form gets imported automatically when the url imports happen. This in turn causes the COUNTRIES = get_countries()
+    variable to be called causing database query errors since migrations haven't been applied yet.
+    Removing COUNTRIES = get_countries() and replacing it with get_form_countries ensures that the get_countries function
+    will now only be explicitly called as opposed to being automatically called.
+    """
     # Only show countries for which data has been submitted
 
     country = forms.ChoiceField(
@@ -22,6 +29,13 @@ class CountryForm(forms.Form):
             return [(code, country) for code, country in get_countries() if code == self.cleaned_data['country']]
 
 class RegionForm(forms.Form):
+    """
+    Due to the system checks added in django 1.9 https://docs.djangoproject.com/en/3.1/releases/1.9/#urls,
+    this form gets imported automatically when the url imports happen. This in turn causes the  REGIONS = get_regions()
+    variable to be called causing database query errors since migrations haven't been applied yet.
+    Removing REGIONS = get_regions() and replacing it with get_form_regions ensures that the get_regions function
+    will now only be explicitly called as opposed to being automatically called.
+    """
 
     region = forms.ChoiceField(
         label='Region',
