d21728eb7d2e4a5e2980a4b59361a8a818bdd14d
Clemence Kyara <clemence@codeforafrica.org>
2020-09-07 09:34:57 +0300

    Customize how we create SpecialQuestions from gsheet

--- gmmp/signals.py
+++ gmmp/signals.py
@@ -1,7 +1,10 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User, Group
-from .models import Monitor
+from django_countries.fields import countries
+from gsheets.signals import sheet_row_processed
+from .models import Monitor, SpecialQuestions
 
 @receiver(post_save, sender=User)
 def create_admin_group(sender, instance, signal, created, **kwargs):
@@ -11,3 +14,14 @@ def create_admin_group(sender, instance, signal, created, **kwargs):
         monitor, created = Monitor.objects.get_or_create(user=instance)
         monitor.save()
         Group.objects.get_or_create(name='%s_admin' % monitor.country)
+
+@receiver(sheet_row_processed, sender=SpecialQuestions)
+def match_special_questions_columns_to_fields(instance=None, created=None, row_data=None, **kwargs):
+    try:
+        instance.country = countries.alpha2(row_data['Country'])
+        instance.question_1 = row_data['Question 1'].strip()
+        instance.question_2 = row_data['Question 2'].strip()
+        instance.question_3 = row_data['Question 3'].strip()
+        instance.save()
+    except (ObjectDoesNotExist, KeyError):
+        pass
