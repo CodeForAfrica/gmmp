569e2c1cbfbfb78b08a60ad092b23acde0ce42be
Clemence Kyara <clemence@codeforafrica.org>
2020-09-17 18:23:41 +0300

    Ensure everything inside $.ready + reogranize

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -1,52 +1,43 @@
 (function($){
-    // module tracks the tab we are currently in.
-    // This enables us to show the contents of the page using the class name module_0, module_1, module_2 etc
-    let module = 0
-    let newspapersheet_form = $('#newspapersheet_form').length;
-    let radiosheet_form = $('#radiosheet_form').length;
-    let televisionsheet_form = $('#televisionsheet_form').length;
-    let internetnewssheet_form = $('#internetnewssheet_form').length;
-    let twittersheet_form = $('#twittersheet_form').length;
+    var set_button_navigation = function() {
+        var last_tab = $('.changeform-tabs li:last-child').hasClass('selected');
+        if(last_tab) {
+            $('input[name=_addanother]').show()
+            $('input[name=_save]').show()
+            $('input[name=_continue]').hide()
+        }else{
+            $('input[name=_addanother]').hide()
+            $('input[name=_save]').hide()
+            $('input[name=_continue]').show()
+        }
+    };
 
-    if(newspapersheet_form > 0 || twittersheet_form > 0 || radiosheet_form > 0 || televisionsheet_form > 0 || internetnewssheet_form > 0){
-        // Rename the save and continue button to Next
-        let cont = $('input[name=_continue]').val('Next Tab')
-        $(document).ready(function() {
-            module = $('.changeform-tabs li.selected').index();
-            let last_tab = $('.changeform-tabs li:last-child').hasClass('selected');
-            if(last_tab) {
-                $('input[name=_addanother]').show()
-                $('input[name=_save]').show()
-                $('input[name=_continue]').hide()
-            }else{
-                $('input[name=_addanother]').hide()
-                $('input[name=_save]').hide()
-                $('input[name=_continue]').show()
-            }
-        });
-        $('body').click(function(e) {
-            setTimeout(function() {
-                module = $('.changeform-tabs li.selected').index();
-                let last_tab = $('.changeform-tabs li:last-child').hasClass('selected');
-                if(last_tab) {
-                $('input[name=_addanother]').show()
-                $('input[name=_save]').show()
-                $('input[name=_continue]').hide()
-                }else{
-                    $('input[name=_addanother]').hide()
-                    $('input[name=_save]').hide()
-                    $('input[name=_continue]').show()
+    $(document).ready(function() {
+        var newspapersheet_form = $('#newspapersheet_form').length > 0;
+        var radiosheet_form = $('#radiosheet_form').length > 0;
+        var televisionsheet_form = $('#televisionsheet_form').length > 0;
+        var internetnewssheet_form = $('#internetnewssheet_form').length > 0;
+        var twittersheet_form = $('#twittersheet_form').length > 0;
+        
+        if(newspapersheet_form || twittersheet_form || radiosheet_form || televisionsheet_form || internetnewssheet_form){
+            // Rename the save and continue button to Next
+            var continueButton$ = $('input[name=_continue]');
+            continueButton$.val('Next Tab')
+            continueButton$.click((function(e) {
+                e.preventDefault();
+                // Move to next tab unless we are on the last tab
+                if ($('.changeform-tabs li.selected:last-child').length === 0){
+                    $('.changeform-tabs li.selected').removeClass('selected').next().addClass('selected');
+                    set_button_navigation();
                 }
-            }, 0)            
-        });
-        $('input[name=_continue]').click((function(e) {
-            e.preventDefault();
-            // Move to next tab unless we are on the last tab
-            if ($('.changeform-tabs li.selected:last-child').length == 0){
-                $('.changeform-tabs li.selected').removeClass('selected').next().addClass('selected');
-                $(`.module_${module}`).removeClass('selected');
-                $(`.module_${module+1}`).addClass('selected');
-            }
-        }));
-    }
+            }));
+            $('.changeform-tabs li').click(function (e) {
+                $('.changeform-tabs li').removeClass('selected');
+                $(e.currentTarget).addClass('selected');
+
+                set_button_navigation();
+            });
+            set_button_navigation();
+        }
+    });
 }(jet.jQuery));
