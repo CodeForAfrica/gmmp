b68c024971420f8ae220f75e3b91bce706cf7f9d
esir <esirkings@gmail.com>
2020-11-04 09:19:53 +0300

    Create a serializer for Newspapers

--- sheets/serializers.py
+++ sheets/serializers.py
@@ -1,6 +1,27 @@
 from rest_framework import serializers
 
-from forms.models import InternetNewsSheet, InternetNewsPerson, InternetNewsJournalist
+from forms.models import (
+            NewspaperSheet, NewspaperPerson, NewspaperJournalist,
+            InternetNewsSheet, InternetNewsPerson, InternetNewsJournalist,
+)
+
+class NewspaperSheetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewspaperSheet
+        fields = '__all__'
+        read_only_fields = ['id']
+
+class NewspaperPersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewspaperPerson
+        fields = '__all__'
+        read_only_fields = ['id']
+
+class NewspaperJournalistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewspaperJournalist
+        fields = '__all__'
+        read_only_fields = ['id']
 
 class InternetNewsSheetSerializer(serializers.ModelSerializer):
     class Meta:
