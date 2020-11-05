26d6d3da5fb036a83db1fccaeaf4f1ec1d1e0942
esir <esirkings@gmail.com>
2020-11-05 07:02:23 +0300

    Update code to support journalist age

--- sheets/coding_info.py
+++ sheets/coding_info.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 
 #coding sheet descriptions
-#dict that indicates how each sheet looks like
+ #dict that indicates how each sheet looks like
 media_dict = {"NewspaperCoding": "NEWSPAPER CODING SHEET",
                   "RadioCoding": "RADIO CODING SHEET",
                   "TelevisionCoding": "TELEVISION CODING SHEET",
@@ -25,45 +25,9 @@ media_dict = {"NewspaperCoding": "NEWSPAPER CODING SHEET",
 
 #Basic Information descriptions
 basic_info = {'Basic Information':"Other", 
-                 'Information de base':'Autre', 
-                 'Información básica':'Otro',
-                 'Informações básicas': 'Outra'}
-                 
-                 
-people_dict = {'NewspaperCoding': {'10': 'sex', '11': 'age', '12': 'occupation', '13': 'function', '14': 'family_role',
-                                   '15': 'victim_or_survivor', '16': 'victim_of', '17': 'survivor_of', '18': 'is_quoted', '19': 'is_photograph',
-                                   '20': 'special_qn_1', '21': 'special_qn_2','22': 'special_qn_3'},
-                'RadioCoding': {'10': 'sex', '11': 'age', '12': 'occupation','13': 'family_role', '14': 'victim_or_survivor',
-                                '15': 'victim_of', '16': 'survivor_of', '17': 'special_qn_1', '18': 'special_qn_2','19': 'special_qn_3'},
-                'TelevisionCoding': {'11': 'sex','12': 'age', '13': 'occupation', '14': 'function','15': 'family_role', '16': 'victim_or_survivor',
-                                     '17': 'victim_of', '18': 'survivor_of', '19': 'special_qn_1','20': 'special_qn_2','21': 'special_qn_3'},
-                'InternetCoding': {'12': 'sex', '13': 'age','14': 'occupation', '15': 'function','16': 'family_role',
-                                   '17': 'victim_or_survivor','18': 'victim_of','19': 'survivor_of', '20': 'is_quoted','21': 'is_photograph',
-                                   '22': 'special_qn_1', '23': 'special_qn_2','24': 'special_qn_3'},
-                'TwitterCoding': {'9': 'sex','10': 'age','11': 'occupation','12': 'function', '13': 'is_photograph',
-                                  '14': 'special_qn_1','15': 'special_qn_2','16': 'special_qn_3'}}
-
-journalist_dict = {'NewspaperCoding': {'9': 'sex'},
-                    'RadioCoding': {'8': 'role', '9': 'sex'},
-                    'TelevisonCoding' :{'8': 'role', '9': 'sex', '10': 'age'},
-                    'InternetCoding' :{'10': 'role', '11': 'sex'},
-                    'TwitterCoding': {'7': 'role', '8': 'sex'}}
-
-sheet_info = {'NewspaperCoding': {'1':'page_number', 'z':'covid19', '2':'topic'	,'3':'scope', '4':'space',
-                                    '5':'equality_rights','6':'about_women','7':'inequality_women','8':'stereotypes',
-                                    '24':'further_analysis', '30':'comments'},
-              'RadioCoding': {'1':'item_number', 'z':'covid19', '2':'topic'	,'3':'scope',
-                                    '4':'equality_rights','5':'about_women','6':'inequality_women','7':'stereotypes',
-                                    '20':'further_analysis', '30':'comments'},
-              'TelevisionCoding': {'1':'item_number', 'z':'covid19', '2':'topic','3':'scope',
-                                    '4':'equality_rights','5':'about_women','6':'inequality_women','7':'stereotypes',
-                                    '22':'further_analysis', '30':'comments'},
-             'InternetCoding': {'1':'webpage_layer_no', 'z':'covid19', '2':'topic'	,'3':'scope', '4':'shared_via_twitter','5':'shared_via_facebook',
-                                    '6':'equality_rights','7':'about_women','8':'inequality_women','9':'stereotypes',
-                                    '25':'further_analysis', '30':'comments'},
-             'TwitterCoding': {'1':'retweet', 'z':'covid19', '2':'topic',
-                                    '3':'equality_rights','4':'about_women','4':'inequality_women','6':'stereotypes',
-                                    '18':'further_analysis', '30':'comments'}}
+              'Information de base':'Autre', 
+              'Información básica':'Otro',
+              'Informações básicas': 'Outra'}
 
 #Basic info translations
 basicinfo_translations = {"country_code":['Código del país','Cód país','Code du pays', 'Country code'],
@@ -81,3 +45,98 @@ basicinfo_translations = {"country_code":['Código del país','Cód país','Code
                           "offline_presence":['Version impresa o "offline"?', 'Presenca offline?','Présence hors ligne?', 'Offline presence?'],
                           "media_name":['Nombre del medio','Nome do veículo', 'Nom du média', 'Media name'],
                           "twitter_handle":['Nombre de usuario de Twitter', 'Usuário do Twitter', "Nom d'utilisateur Twitter", 'Twitter handle']}
+                    
+
+#section mapping
+newspaper_qs = {'story':{'1':'page_number', 'z':'covid19', '2':'topic'	,'3':'scope', '4':'space'},
+             'analysis': {'5':'equality_rights','6':'about_women','7':'inequality_women','8':'stereotypes'},
+             'journalists':{'9':'sex'},
+             'people':{'10':'sex','11':'age','12':'occupation','13':'function','14':'family_role','15':'victim_or_survivor',	
+                       '16':'victim_of','17':'survivor_of','18':'is_quoted'	,'19':'is_photograph','20':'special_qn_1','21':'special_qn_2','22':'special_qn_3'},
+             'further_analysis':{'24':'further_analysis'},
+             'comments':{'30':'comments'}}
+
+
+radio_qs = {'story':{'1':'item_number', 'z':'covid19', '2':'topic'	,'3':'scope'},
+         'analysis': {'4':'equality_rights','5':'about_women','6':'inequality_women','7':'stereotypes'},
+         'journalists':{'8':'role','9':'sex'},
+         'people':{'10':'sex','11':'age','12':'occupation','13':'family_role','14':'victim_or_survivor',	
+                '15':'victim_of','16':'survivor_of','17':'special_qn_1','18':'special_qn_2','19':'special_qn_3'},
+         'further_analysis':{'20':'further_analysis'},
+         'comments':{'30':'comments'}}
+
+television_qs = {'story':{'1':'item_number', 'z':'covid19', '2':'topic'	,'3':'scope'},
+         'analysis': {'4':'equality_rights','5':'about_women','6':'inequality_women','7':'stereotypes'},
+         'journalists':{'8':'role','9':'sex', '10':'age'},
+         'people':{'11':'sex','12':'age','13':'occupation','14':'function','15':'family_role','16':'victim_or_survivor',	
+                '17':'victim_of','18':'survivor_of','19':'special_qn_1','20':'special_qn_2','21':'special_qn_3'},
+         'further_analysis':{'22':'further_analysis'},
+         'comments':{'30':'comments'}}
+
+
+internet_qs = {'story':{'1':'webpage_layer_no', 'z':'covid19', '2':'topic'	,'3':'scope', '4':'shared_via_twitter','5':'shared_via_facebook'},
+            'analysis': {'6':'equality_rights','7':'about_women','8':'inequality_women','9':'stereotypes'},
+            'journalists':{'10':'role','11':'sex'},
+            'people':{'12':'sex','13':'age','14':'occupation','15':'function','16':'family_role','17':'victim_or_survivor',
+                   '18':'victim_of','19':'survivor_of','20':'is_quoted','21':'is_photograph','22':'special_qn_1','23':'special_qn_2','24':'special_qn_3'},
+            'further_analysis':{'25':'further_analysis'},
+            'comments':{'30':'comments'}}
+
+twitter_qs = {'story':{'1':'retweet', 'z':'covid19', '2':'topic'},
+            'analysis': {'3':'equality_rights','4':'about_women','5':'inequality_women','6':'stereotypes'},
+            'journalists':{'7':'role','8':'sex'},
+            'people':{'9':'sex','10':'age','11':'occupation','12':'function','13':'is_photograph','14':'special_qn_1','15':'special_qn_2','16':'special_qn_3'},
+            'further_analysis':{'18':'further_analysis'},
+            'comments':{'30':'comments'}}
+
+
+question_models = OrderedDict([
+    ('Print', newspaper_qs),
+    ('Radio', radio_qs),
+    ('Televison', television_qs),
+    ('Internet', internet_qs),
+    ('Twitter', twitter_qs)
+])
+
+
+#Coding sheetname mapping
+sheetname_mapping = {"Print": ['NewspaperCoding', 'CODAGE POUR JOURNAUX', 'CODIFICACIÓN PARA PERIÓDICOS', 'CODIFICAÇÃO DE JORNAIS'],
+                     "Radio":['RadioCoding', "CODAGE D'EMMISSION RADIO",'CODIFICACIÓN PARA RADIO','CODIFICAÇÃO DE RADIO'],
+                     "Television": ['TelevisionCoding','CODAGE D’ÉMISSION DE TÉLÉVISION','CODIFICACIÓN PARA TELEVISIÓN','CODIFICAÇÃO DE TELEVISÃO'],
+                     "Internet": ['InternetCoding', 'CODAGE POUR INTERNET','CODIFICACIÓN PARA INTERNET','CODIFICAÇÃO INTERNET'],
+                     "Twitter": ['TwitterCoding', 'CODAGE POUR TWITTER','CODIFICACIÓN PARA TWITTER','CODIFICAÇÃO TWITTER']}                
+
+people_dict = {'NewspaperCoding': {'10': 'sex', '11': 'age', '12': 'occupation', '13': 'function', '14': 'family_role',
+                                   '15': 'victim_or_survivor', '16': 'victim_of', '17': 'survivor_of', '18': 'is_quoted', '19': 'is_photograph',
+                                   '20': 'special_qn_1', '21': 'special_qn_2','22': 'special_qn_3'},
+                'RadioCoding': {'10': 'sex', '11': 'age', '12': 'occupation','13': 'family_role', '14': 'victim_or_survivor',
+                                '15': 'victim_of', '16': 'survivor_of', '17': 'special_qn_1', '18': 'special_qn_2','19': 'special_qn_3'},
+                'TelevisionCoding': {'11': 'sex','12': 'age', '13': 'occupation', '14': 'function','15': 'family_role', '16': 'victim_or_survivor',
+                                     '17': 'victim_of', '18': 'survivor_of', '19': 'special_qn_1','20': 'special_qn_2','21': 'special_qn_3'},
+                'InternetCoding': {'12': 'sex', '13': 'age','14': 'occupation', '15': 'function','16': 'family_role',
+                                   '17': 'victim_or_survivor','18': 'victim_of','19': 'survivor_of', '20': 'is_quoted','21': 'is_photograph',
+                                   '22': 'special_qn_1', '23': 'special_qn_2','24': 'special_qn_3'},
+                'TwitterCoding': {'9': 'sex','10': 'age','11': 'occupation','12': 'function', '13': 'is_photograph',
+                                  '14': 'special_qn_1','15': 'special_qn_2','16': 'special_qn_3'}}
+
+journalist_dict = {'NewspaperCoding': {'9': 'sex'},
+                    'RadioCoding': {'8': 'role', '9': 'sex'},
+                    'TelevisonCoding' :{'8': 'role', '9': 'sex', '10': 'age'},
+                    'InternetCoding' :{'10': 'sex', '11': 'age'},
+                    'TwitterCoding': {'7': 'sex', '8': 'age'}}
+
+sheet_info = {'NewspaperCoding': {'1':'page_number', 'z':'covid19', '2':'topic'	,'3':'scope', '4':'space',
+                                    '5':'equality_rights','6':'about_women','7':'inequality_women','8':'stereotypes',
+                                    '24':'further_analysis', '30':'comments'},
+              'RadioCoding': {'1':'item_number', 'z':'covid19', '2':'topic'	,'3':'scope',
+                                    '4':'equality_rights','5':'about_women','6':'inequality_women','7':'stereotypes',
+                                    '20':'further_analysis', '30':'comments'},
+              'TelevisionCoding': {'1':'item_number', 'z':'covid19', '2':'topic','3':'scope',
+                                    '4':'equality_rights','5':'about_women','6':'inequality_women','7':'stereotypes',
+                                    '22':'further_analysis', '30':'comments'},
+             'InternetCoding': {'1':'webpage_layer_no', 'z':'covid19', '2':'topic'	,'3':'scope', '4':'shared_via_twitter','5':'shared_via_facebook',
+                                    '6':'equality_rights','7':'about_women','8':'inequality_women','9':'stereotypes',
+                                    '25':'further_analysis', '30':'comments'},
+             'TwitterCoding': {'1':'retweet', 'z':'covid19', '2':'topic',
+                                    '3':'equality_rights','4':'about_women','5':'inequality_women','6':'stereotypes',
+                                    '18':'further_analysis', '30':'comments'}}
