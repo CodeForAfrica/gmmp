a45cc1d251e81e1bfe29b11655064b4de2943393
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2020-12-07 12:08:02 +0100

    extract common code into base_import, recodes, and canon files

---
+++ reports/utils/canon.py
@@ -0,0 +1,8 @@
+from reports.utils.recodes import recode
+
+def canon(key):
+    if key:
+        key = key.replace(u'\u2026', '')
+        key = recode(key.strip())
+        return key.strip().lower()
+    return ''
