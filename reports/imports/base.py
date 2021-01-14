90e8654c6015939bb4bbd33b71a2f104b23bee48
_ Kilemensi <clemence@codeforafrica.org>
2021-01-14 12:40:01 +0300

    Remove unused method & format

--- reports/imports/base.py
+++ reports/imports/base.py
@@ -18,7 +18,7 @@ def v(v):
     if not isinstance(v, basestring):
         return v
 
-    m = re.match(r'^(\d+(\.\d+)?)(%)?$', v)
+    m = re.match(r"^(\d+(\.\d+)?)(%)?$", v)
     if m:
         if not m.group(2):
             # int
@@ -31,8 +31,20 @@ def v(v):
             v = v / 100.0
     return v
 
+
 class BaseImport(object):
-    def slurp_secondary_col_table(self, ws, data, col_start, cols_per_group, cols, row_end, row_start=5, major_col_heading_row=4, row_heading_col=5):
+    def slurp_secondary_col_table(
+        self,
+        ws,
+        data,
+        col_start,
+        cols_per_group,
+        cols,
+        row_end,
+        row_start=5,
+        major_col_heading_row=4,
+        row_heading_col=5,
+    ):
         """
         Get values from a table with two levels of column headings.
 
@@ -44,13 +56,34 @@ class BaseImport(object):
         row3
         """
         for icol in range(col_start, col_start + cols * cols_per_group, cols_per_group):
-            major_col_heading = canon(ws.cell(column=icol, row=major_col_heading_row).value)
+            major_col_heading = canon(
+                ws.cell(column=icol, row=major_col_heading_row).value
+            )
             major_col_data = {}
             data[major_col_heading] = major_col_data
 
-            self.slurp_table(ws, major_col_data, icol, icol + cols_per_group - 1, row_end, row_start=row_start, col_heading_row=major_col_heading_row + 1, row_heading_col=row_heading_col)
+            self.slurp_table(
+                ws,
+                major_col_data,
+                icol,
+                icol + cols_per_group - 1,
+                row_end,
+                row_start=row_start,
+                col_heading_row=major_col_heading_row + 1,
+                row_heading_col=row_heading_col,
+            )
 
-    def slurp_table(self, ws, data, col_start, col_end, row_end, row_start=7, col_heading_row=5, row_heading_col=2):
+    def slurp_table(
+        self,
+        ws,
+        data,
+        col_start,
+        col_end,
+        row_end,
+        row_start=7,
+        col_heading_row=5,
+        row_heading_col=2,
+    ):
         """
         Grab values from a simple table with column and row titles.
 
@@ -71,8 +104,20 @@ class BaseImport(object):
                 row_heading = canon(ws.cell(column=row_heading_col, row=irow).value)
                 col_data[row_heading] = v(ws.cell(column=icol, row=irow).value)
 
-    def slurp_year_grouped_table(self, ws, all_data, col_start, cols_per_group, cols, row_end, row_start=5, year_heading_row=4, col_heading_row=3, row_heading_col=5,
-                                skip_years=[]):
+    def slurp_year_grouped_table(
+        self,
+        ws,
+        all_data,
+        col_start,
+        cols_per_group,
+        cols,
+        row_end,
+        row_start=5,
+        year_heading_row=4,
+        col_heading_row=3,
+        row_heading_col=5,
+        skip_years=[],
+    ):
         """
         Slurp a table where each category contains a range of years.
 
@@ -88,9 +133,9 @@ class BaseImport(object):
 
             for iyear in range(icol, icol + cols_per_group):
                 year = ws.cell(column=iyear, row=year_heading_row).value
-                if year in ['N', 'N-F']:
+                if year in ["N", "N-F"]:
                     year = 2010
-                    effective_col_heading = canon('N')
+                    effective_col_heading = canon("N")
                 else:
                     year = int(year)
                     effective_col_heading = col_heading
@@ -100,19 +145,7 @@ class BaseImport(object):
                     col_data = data.setdefault(effective_col_heading, {})
 
                     for irow in range(row_start, row_end + 1):
-                        row_heading = canon(ws.cell(column=row_heading_col, row=irow).value)
+                        row_heading = canon(
+                            ws.cell(column=row_heading_col, row=irow).value
+                        )
                         col_data[row_heading] = v(ws.cell(column=iyear, row=irow).value)
-
-    def import_sheet_5(self, ws, data, col_start, col_heading_row, row_start, row_end, row_heading_col):
-        self.slurp_year_grouped_table(
-            ws,
-            data,
-            col_start=col_start,
-            cols=1,
-            cols_per_group=5,
-            year_heading_row=4,
-            col_heading_row=col_heading_row,
-            row_start=row_start,
-            row_end=row_end,
-            row_heading_col=row_heading_col
-        )
