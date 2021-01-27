a26cb3e69b7fde28506ecf9af602ba5c0968299b
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2021-01-27 13:26:36 +0100

    import sheet 59

--- reports/historical/_gmmp_2015_importer.py
+++ reports/historical/_gmmp_2015_importer.py
@@ -372,3 +372,10 @@ class GMMP2015ReportImporter(BaseReportImporter):
                 (2015, 3, 11, 7, 120),
             ]
         )
+
+    def import_59(self, sheet_info):
+        return self.import_grid(
+            [
+                (2015, 3, 6, 7, 10),
+            ]
+        )
