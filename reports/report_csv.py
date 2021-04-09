c5eaf43fb2c34bae85ebef4c9e7a4db7bd40ce41
esir <esirkings@gmail.com>
2021-04-09 05:26:55 +0300

    Adds module for csv reports. Includes functions for ws_05 and ws_09

---
+++ reports/report_csv.py
@@ -0,0 +1,27 @@
+import csv
+from .report_details import *  # noqa
+from forms.modelutils import (TOPICS, )
+
+def get_gender(gender_id):
+    return "Female" if gender_id==1 else "Male"
+
+def ws_05_csv(writer, counts_list, row):
+    for media_type in row:
+        for topic in row[media_type]:
+            topic_name = [x for x in MAJOR_TOPICS if x[0] == topic[1]][0][1]
+            gender = get_gender(topic[0])
+
+            writer.writerow({'Topic': topic_name, 'Gender': gender, 'Medium': media_type, 'Count': row[media_type][topic]})
+
+def ws_09_csv(writer, counts_list, row):
+    topic = [y for x in TOPICS for y in x[1] if y[0]==row[1]][0][1]
+    gender = get_gender(row[0])
+
+    writer.writerow({'Topic': topic, 'Gender': gender, 'Count': counts_list[row]})
+
+def generate_csv(csv_name, fieldnames, counts_list, func):
+    with open(f'csv/{csv_name}.csv', 'a+') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in (counts_list):
+            func(writer, counts_list, row)
