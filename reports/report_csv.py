423c513052dbc37ac2159e967cffb8f9dd1796ee
esir <esirkings@gmail.com>
2021-04-09 05:44:31 +0300

    Adds csv function for worksheet 15

--- reports/report_csv.py
+++ reports/report_csv.py
@@ -1,6 +1,6 @@
 import csv
 from .report_details import *  # noqa
-from forms.modelutils import (TOPICS, )
+from forms.modelutils import (TOPICS, FUNCTION, )
 
 def get_gender(gender_id):
     return "Female" if gender_id==1 else "Male"
@@ -18,6 +18,14 @@ def ws_09_csv(writer, counts_list, row):
     gender = get_gender(row[0])
 
     writer.writerow({'Topic': topic, 'Gender': gender, 'Count': counts_list[row]})
+
+def ws_15_csv(writer, counts_list, row):
+    function = [func[1] for func in FUNCTION if func[0] == row[1]]
+    if function:
+        function = function[0][4:] # [4:] is used to remove the (number) part
+        gender = get_gender(row[0])
+
+        writer.writerow({'Function': function, 'Gender': gender, 'Count': counts_list[row]})
 
 def generate_csv(csv_name, fieldnames, counts_list, func):
     with open(f'csv/{csv_name}.csv', 'a+') as csv_file:
