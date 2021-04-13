0139f7875f97bd3bc4868c6e94ae085a04fe5756
esir <esirkings@gmail.com>
2021-04-13 12:22:58 +0300

    Use clean_title to cleanup text

--- reports/report_dataset.py
+++ reports/report_dataset.py
@@ -28,18 +28,20 @@ def ws_06_dataset(writer, counts_list, row, **kwargs):
         writer.writerow({'Topic': topic_name, 'Gender': gender, 'Region': row, 'Count': count})
 
 def ws_09_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     topic = [y for x in TOPICS for y in x[1] if y[0]==row[1]][0][1]
     gender = get_gender(row[0])
 
-    writer.writerow({'Topic': topic, 'Gender': gender, 'Count': counts_list[row]})
+    writer.writerow({'Topic': clean_title(topic), 'Gender': gender, 'Count': counts_list[row]})
 
 def ws_15_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
     function = [func[1] for func in FUNCTION if func[0] == row[1]]
     if function:
-        function = function[0][4:] # [4:] is used to remove the (number) part
         gender = get_gender(row[0])
 
-        writer.writerow({'Function': function, 'Gender': gender, 'Count': counts_list[row]})
+        writer.writerow({'Function': clean_title(function), 'Gender': gender, 'Count': counts_list[row]})
 
 def ws_28b_dataset(writer, counts_list, row, **kwargs):
     medium = kwargs['medium']
@@ -65,30 +67,40 @@ def ws_28c_dataset(writer, counts_list, row, **kwargs):
         writer.writerow({'Region': region, 'Medium': medium, 'Gender': gender, 'Count': counts_list[row][presenter]})
 
 def ws_30_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     for reporter in counts_list[row]:
         gender = get_gender(reporter[0])
         topic = [t for t in MAJOR_TOPICS if t[0] == reporter[1]][0][1]
-        writer.writerow({'Region': row, 'Topic': topic, 'Gender': gender, 'Count': counts_list[row][reporter]})
+        writer.writerow({'Region': row, 'Topic': clean_title(topic), 'Gender': gender, 'Count': counts_list[row][reporter]})
 
 def ws_38_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     topic = [t for t in MAJOR_TOPICS if t[0] == row[1]][0][1]
-    writer.writerow({'Topic': topic, 'Answer': row[0], 'Count': counts_list[row]})
+    writer.writerow({'Topic': clean_title(topic), 'Answer': row[0], 'Count': counts_list[row]})
 
 def ws_41_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     all_topics = [y for x in TOPICS for y in x[1]]
     topic = [x[1] for x in all_topics if x[0] == row[1]][0]
-    writer.writerow({'Topic': topic[4:].strip(), 'Answer': row[0], 'Count': counts_list[row]})
+    writer.writerow({'Topic': clean_title(topic), 'Answer': row[0], 'Count': counts_list[row]})
 
 def ws_47_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     topic = [t for t in MAJOR_TOPICS if t[0] == row[1]][0][1]
     answer = 'Agree' if row[0] == 1 else 'Disagree'
-    writer.writerow({'Topic': topic, 'Answer': answer, 'Count': counts_list[row]})
+    writer.writerow({'Topic': clean_title(topic), 'Answer': answer, 'Count': counts_list[row]})
 
 def ws_48_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     topic = [t for t in MAJOR_TOPICS if t[0] == row[1]][0][1]
     answer = 'Agree' if row[0] == 1 else 'Disagree'
     gender = get_gender(kwargs['gender'])
-    writer.writerow({'Topic': topic, 'Gender': gender, 'Answer': answer, 'Count': counts_list[row]})
+    writer.writerow({'Topic': clean_title(topic), 'Gender': gender, 'Answer': answer, 'Count': counts_list[row]})
 
 def ws_83_dataset(writer, counts_list, row, **kwargs):
     regions = kwargs['regions']
@@ -99,29 +111,34 @@ def ws_83_dataset(writer, counts_list, row, **kwargs):
         writer.writerow({'Topic': row, 'Region': region, 'Gender': gender, 'Count': count})
 
 def ws_85_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     regions = kwargs['regions']
     function, region = row
     region = [x[1] for x in regions if x[0] == region][0]
     function = [func[1] for func in FUNCTION if func[0] == function]
     if function:
-        function = function[0][4:] # [4:] is used to remove the (number) part
-        writer.writerow({'Region': region, 'Function': function, 'Count': counts_list[row]})
+        writer.writerow({'Region': region, 'Function': clean_title(function), 'Count': counts_list[row]})
 
 def ws_92_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     topic, agree_disagree = row
     count = counts_list[row]
     region = kwargs['region']
     topic = [t for t in MAJOR_TOPICS if t[0] == topic][0][1]
     agree_disagree = 'Agree' if agree_disagree == 1 else 'Disagree'
-    writer.writerow({'Region': region, 'Topic': topic, 'Answer': agree_disagree, 'Count': count})
+    writer.writerow({'Region': region, 'Topic': clean_title(topic), 'Answer': agree_disagree, 'Count': count})
 
 def ws_93_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     topic, yes_no = row
     count = counts_list[row]
     region = kwargs['region']
     topic = [t for t in MAJOR_TOPICS if t[0] == topic][0][1]
     yes_no = 'Yes' if yes_no == 'Y' else 'No'
-    writer.writerow({'Region': region, 'Topic': topic, 'Answer': yes_no, 'Count': count})
+    writer.writerow({'Region': region, 'Topic': clean_title(topic), 'Answer': yes_no, 'Count': count})
 
 def ws_97_dataset(writer, counts_list, row, **kwargs):
     regions = kwargs['regions']
@@ -133,26 +150,32 @@ def ws_97_dataset(writer, counts_list, row, **kwargs):
         writer.writerow({'Region': region, 'Topic': row, 'Agree/Disagree': agree_disagree, 'Count': count})
 
 def ws_100_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     for answer_topic in counts_list[row]:
         answer, topic = answer_topic
         yes_no = 'Yes' if answer == 'Y' else 'No'
         topic = [t for t in MAJOR_TOPICS if t[0] == topic][0][1]
         count = counts_list[row][answer_topic]
-        writer.writerow({'Topic': topic, 'Medium': row, 'Yes/No': yes_no, 'Count': count})
+        writer.writerow({'Topic': clean_title(topic), 'Medium': row, 'Yes/No': yes_no, 'Count': count})
 
 def ws_101_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     gender, topic = row
     gender = get_gender(gender)
     topic = [t for t in MAJOR_TOPICS if t[0] == topic][0][1]
     count = counts_list[row]
-    writer.writerow({'Topic': topic, 'Gender': gender, 'Count': count})
+    writer.writerow({'Topic': clean_title(topic), 'Gender': gender, 'Count': count})
 
 def ws_102_dataset(writer, counts_list, row, **kwargs):
+    from reports.report_builder import clean_title
+
     agree_disagree, topic = row
     topic = [t for t in MAJOR_TOPICS if t[0] == topic][0][1]
     agree_disagree = 'Agree' if agree_disagree == 1 else 'Disagree'
     count = counts_list[row]
-    writer.writerow({'Topic': topic, 'Agree/Disagree': agree_disagree, 'Count': count})
+    writer.writerow({'Topic': clean_title(topic), 'Agree/Disagree': agree_disagree, 'Count': count})
 
 def ws_104_dataset(writer, counts_list, row, **kwargs):
     function, gender = row
